4ebdd9ea532df10b4da67fdb4e171c70d4d8471f
osamaahmedalsharabi <osamaahmed70088@gmail.com>
2024-12-02 04:04:33 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/third_note_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.third_note_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:third_note_app/views/home_view.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HomeView(),
+    );
+  }
+}
